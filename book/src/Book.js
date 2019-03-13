dbbf7acaa9e80276d69a4598eef57e9cdd6c6556
Jeremy Tsukasa Zimmerman <jtzimmerm@gmail.com>
2019-03-12 21:05:44 -0400

    Just CSS Left

--- book/src/Book.js
+++ book/src/Book.js
@@ -4,7 +4,10 @@ class Book extends Component{
     render() {
         return (
             <div>
-                <h1>First Success!!!</h1>
+                <h1>The Stinky Cheese Man: and other Fairly Stupid Tales</h1>
+                <h6>By Jon Scieszka & Lane Smith</h6>
+                <img src="https://images-na.ssl-images-amazon.com/images/I/611tCcAdSSL._SX411_BO1,204,203,200_.jpg"/>
+                <p>If geese had graves, Mother Goose would be rolling in hers. The Stinky Cheese Man and Other Fairly Stupid Tales retells--and wreaks havoc on--the allegories we all thought we knew by heart. In these irreverent variations on well-known themes, the ugly duckling grows up to be an ugly duck, and the princess who kisses the frog wins only a mouthful of amphibian slime. The Stinky Cheese Man deconstructs not only the tradition of the fairy tale but also the entire notion of a book. Our naughty narrator, Jack, makes a mockery of the title page, the table of contents, and even the endpaper by shuffling, scoffing, and generally paying no mind to structure. Characters slide in and out of tales; Cinderella rebuffs Rumpelstiltskin, and the Giant at the top of the beanstalk snacks on the Little Red Hen. There are no lessons to be learned or morals to take to heart--just good, sarcastic fun that smart-alecks of all ages will love.</p>
             </div>
         )
     }
